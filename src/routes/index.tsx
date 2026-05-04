64fc4e632befbb128fb2ecbf45264d41060c05ff
jdcoalresources-del <jdcoalresources@gmail.com>
2026-05-04 14:53:43 +0530

    578

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -575,11 +575,7 @@ function ProductsSection() {
       >
         {/* Header */}
         <div className="text-center mb-16">
-          <img 
-    src="/logo.png" 
-    alt="JD Coal Logo" 
-    className="mx-auto mb-4 h-16"
-  />
+<img src="images/logo.png" alt="Logo" height="50"></img>
           <div
             className="text-xs font-bold tracking-widest uppercase mb-4"
             style={{ color: '#D4A017' }}
